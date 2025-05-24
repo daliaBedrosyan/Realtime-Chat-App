7f32aeb3789dc7c8f92ef98c1531538b66127116
dalia9999 <daliabedrosyan47@gmail.com>
2025-05-24 20:54:39 +0300

    Add user signup functionality in the controller and save the data in the database

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,41 @@
-export const signup = (req, res) => {
-    res.send("Signup page");
+import { User } from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    try {
+        if (!fullName || !username || !password || !confirmPassword || !gender) {
+            return res.status(400).json({ success: false, message: "All fields are required" });
+        }
+        if (password !== confirmPassword) {
+            return res.status(400).json({ success: false, message: "Passwords do not match" });
+        }
+
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ success: false, message: "User already exists" });
+        }
+
+        const userGender = gender === 'male' ? 'boy' : 'girl';
+        const profilePic = `https://avatar.iran.liara.run/public/${userGender}?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: profilePic,
+        });
+        await newUser.save();
+
+        const { password: _, __v, ...userData } = newUser.toObject();
+
+        res.status(201).json(userData);
+
+    } catch (error) {
+        console.log("Error in signup:", error);
+        return res.status(500).json({ success: false, message: "Internal server error" });
+    }
 }
 
 export const login = (req, res) => {
