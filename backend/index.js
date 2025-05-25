800eb6e4503c8d3ed6726e062befc47113dca7f0
dalia9999 <daliabedrosyan47@gmail.com>
2025-05-26 00:03:23 +0300

    Add message send/get routes and JWT auth middleware

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import { connectDB } from './db/connectDB.js';
 import { authRoutes } from './routes/auth.route.js';
+import { messageRoutes } from './routes/message.route.js';
 
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 app.listen(PORT, () => {
     connectDB();
