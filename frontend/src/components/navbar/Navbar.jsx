768e30839302b83723210308184aa9c14d2459ba
dalia9999 <daliabedrosyan47@gmail.com>
2025-06-02 23:10:06 +0300

    Fix Vibely button to lead to the home page and change document title to Vibely

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,10 +3,9 @@ const Navbar = () => {
         <nav className="border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
                 <img src="/vibely.png" className="h-8" alt="Vibely Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap bg-gradient-to-r from-blue-700 to-red-900 bg-clip-text text-transparent font-serif mx-5">Vibely</span>
+                <span className="btn btm-nav-label self-center text-2xl font-semibold whitespace-nowrap bg-gradient-to-r from-blue-700 to-red-900 bg-clip-text text-transparent font-serif mx-5"><a href="/">Vibely</a></span>
             </div>
         </nav>
-
     )
 }
 
