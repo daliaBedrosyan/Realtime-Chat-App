ae0eb7079d5112c4473cd9aebf8f6e8a8055dbb0
dalia9999 <daliabedrosyan47@gmail.com>
2025-06-09 11:15:33 +0300

    Implement UI design for MessageInput and NoChatSelected Components in the MessageContainer

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,17 +1,24 @@
+import MessageInput from "./MessageInput"
 import Messages from "./Messages"
+import NoChatSelected from "./NoChatSelected";
 
 const MessageContainer = () => {
+
+    const noChatSelected = true;
+
     return (
         <div className="md:min-w-[450px] flex flex-col">
-            <>
-                {/* <Header /> */}
-                <div className="bg-slate-500 px-4 py-2 mb-2">
-                    <span className="label-text text-white">To:</span>{" "}
-                    <span className="text-slate-700 font-bold">John Doe</span>
-                </div>
-                <Messages />
-                {/* <MessageInput /> */}
-            </>
+            {noChatSelected ? <NoChatSelected /> : (
+                <>
+                    {/* <Header /> */}
+                    <div className="bg-slate-500 px-4 py-2 mb-2">
+                        <span className="label-text text-white">To:</span>{" "}
+                        <span className="text-slate-700 font-bold">John Doe</span>
+                    </div>
+                    <Messages />
+                    <MessageInput />
+                </>
+            )}
         </div>
     )
 }
